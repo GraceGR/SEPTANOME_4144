7a1c96a292cf1261c8fef7a4804db632ad90b7df
AndreaCroc <46070171+AndreaCroc@users.noreply.github.com>
2019-10-25 14:00:48 +0200

    commentaires fin de jeu + affichage
    Sophie et Andrea

--- ALIA/ALIA/Puissance4.pl
+++ ALIA/ALIA/Puissance4.pl
@@ -14,18 +14,22 @@ init :- jouer([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],
                    [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]], 1,
                    [[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6]]).
 
-%Récupérer l'élément d'un liste à l'indice i
+%Récupérer l'élément d'un liste à l'indice N1 et le résultat va dans X
+%Récuperer les indices de l'element X et le resultat va dans N1
 trouverElement(1,X,[X|_]).
 trouverElement(N1,X,[_|L]) :- trouverElement(N,X,L), N1 is N+1.
 
-%Récupérer un élement d'une liste à deux dimensions à l'indice [I][J]
+%Récupérer un élement d'une liste L à deux dimensions à l'indice [I][J]et le résultat va dans Y
 % trouverElement2Dimensions(I,J,Y,L):-trouverElement(I,X,L),trouverElement(J,Y,X).
 trouverElement2Dimensions(I,J,Y,L):- nth0(I,L,X),nth0(J,X,Y).
 
 %%Affichage du plateau
+%Afficher les séparations entre les lignes de la grille
 afficherSeparation() :- write('|---+---+---+---+---+---+---|'),write('\n').
+%Afficher tous les elements X1 d'une ligne L1 et si on arrive à la fin de la ligne, on saute une ligne
 afficherLigne([]) :- write('\n').
 afficherLigne([X1|L1]) :- write(' '), write(X1), write(' '),write('|'), afficherLigne(L1).
+%Afficher les lignes L de la grille G
 afficherPlateau([]).
 afficherPlateau([L|G]) :- write('|'),afficherLigne(L), afficherSeparation(), afficherPlateau(G).
 
@@ -100,30 +104,41 @@ gagnant(J) :-  write('Game Over. Gagnant :'), writeln(J).
 %%%Conditions de victoire
 
 %%Victoire à l'horizontale
+%Regarder s'il y a une combinaison de 4 jetons J sur une même ligne, le résultat est renvoyé dans le dernier argument
 victoireLigne([J,J,J,J,_,_,_],J,1).
 victoireLigne([_,J,J,J,J,_,_],J,1).
 victoireLigne([_,_,J,J,J,J,_],J,1).
 victoireLigne([_,_,_,J,J,J,J],J,1).
 victoireLigne(_,_,0).
+%Parcours toutes les lignes L de la grille G pour trouver s'il y a une victoire pour le joueur J
 victoireHorizontale([],_,_).
 victoireHorizontale([L|_],J):- victoireLigne(L,J,R),R==1.
 victoireHorizontale([L|G],J):- victoireLigne(L,J,R),R==0,victoireHorizontale(G,J).
 
 
 %%Victoire à la verticale
+%Regarder s'il y a une combinaison de 4 jetons J sur une même colonne
 victoireColonne([J,J,J,J,_,_],J).
 victoireColonne([_,J,J,J,J,_],J).
 victoireColonne([_,_,J,J,J,J],J).
 
+%Récuperer chaque element à l'indice I de toutes les lignes L de la grille G pour en faire une colonne R
 recupererCol([],_,[]).
 recupererCol([L|G],I,[R1|R]):-trouverElement(I,R1,L),recupererCol(G,I,R).
 
+%Récuperer une colonne après l'autre et regarder si elle est gagnante, sinon continuer avec la colonne suivante
 victoireVerticale([],_,_) :- fail.
 victoireVerticale(G,I,J) :- recupererCol(G,I,R), I1 is I+1, (victoireColonne(R,J);victoireVerticale(G,I1,J)).
 
 
 %%Victoire à la diagonale
-%%Victoire en diagonale sens 1
+%%Victoire en diagonale sens 1 : haut droit et bas gauche
+%Parcourir la grille G et récupérer l'element à l'indice [I][K]. Si il est égal à J alors, on incrémente notre compteur CMPT et on poursuit 
+%la recherche dans la même diagonale. Sinon, on met CMPT à 0 et on continue dans la même diagonale. Dans les deux cas, on incremente CMTI et CMTK
+%qui correspondent au nombre de coups faits dans une même diagonale.
+%Si CMTP = 4, alors la diagonale est victorieuse et on arrête.
+%Si on a fini le parcours de toute la grille, on arrete et on retourne fail.
+%Lorsque on dépasse les limites de la grille, on se replace au début de la diagonale suivante.
 victoireDiagonale1(_,_,_,_,4,_,_).
 victoireDiagonale1(_,I,K,_,_,_,_):- I>5,K>6,fail.
 victoireDiagonale1(G,I,K,J,CMPT,CMTI,CMTK):- I=<5,K=<6,K>=0,trouverElement2Dimensions(I,K,Y,G),Y==J,CMPT1 is CMPT + 1, I1 is I+1, K1 is K-1,CMTI1 is CMTI +1, CMTK1 is CMTK +1,victoireDiagonale1(G,I1,K1,J,CMPT1,CMTI1,CMTK1).
@@ -132,7 +147,13 @@ victoireDiagonale1(G,I,K,J,_,CMTI,CMTK):- K<0,K1 is CMTK + 1,I1 is I-CMTI,victoi
 victoireDiagonale1(G,I,K,J,_,_,_):- K==7,I1 is I+1,victoireDiagonale1(G,I1,0,J,0,0,0).
 victoireDiagonale1(G,I,_,J,_,CMTI,_):- I==6, I1 is I-CMTI+1,victoireDiagonale1(G,I1,0,J,0,0,0).
 
-%%Victoire en diagonale sens 2
+%%Victoire en diagonale sens 2 : haut gauche et bas droit
+%Parcourir la grille G et récupérer l'element à l'indice [I][K]. Si il est égal à J alors, on incrémente notre compteur CMPT et on poursuit 
+%la recherche dans la même diagonale. Sinon, on met CMPT à 0 et on continue dans la même diagonale. Dans les deux cas, on incremente CMTI et CMTK
+%qui correspondent au nombre de coups faits dans une même diagonale.
+%Si CMTP = 4, alors la diagonale est victorieuse et on arrête.
+%Si on a fini le parcours de toute la grille, on arrete et on retourne fail.
+%Lorsque on dépasse les limites de la grille, on se replace au début de la diagonale suivante.
 victoireDiagonale2(_,_,_,_,4,_,_).
 victoireDiagonale2(_,I,K,_,_,_,_):- I>5,K<0,fail.
 victoireDiagonale2(G,I,K,J,CMPT,CMTI,CMTK):- I=<5,K=<6,K>=0,trouverElement2Dimensions(I,K,Y,G),Y==J,CMPT1 is CMPT + 1, I1 is I+1, K1 is K+1,CMTI1 is CMTI +1, CMTK1 is CMTK +1,victoireDiagonale2(G,I1,K1,J,CMPT1,CMTI1,CMTK1).
@@ -142,7 +163,8 @@ victoireDiagonale2(G,I,K,J,_,CMTI,CMTK):- K==7,I1 is I-CMTI,K1 is K-CMTK-1,victo
 victoireDiagonale2(G,I,_,J,_,CMTI,_):- I==6, I1 is I-CMTI+1,victoireDiagonale2(G,I1,6,J,0,0,0).
 
 
-%%%Grille pleine
+%%Grille pleine
+%On regarde si la liste des coups possibles LCP est vide. Si c'est le cas la partie est terminée.
 ligneVide([]).
 grillePleine([]).
 grillePleine([X|LCP]) :- ligneVide(X), grillePleine(LCP).
