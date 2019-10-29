e999cc9f42dbdd7a1744c697d583267793124166
FatimaW <50332888+FatimaW@users.noreply.github.com>
2019-10-29 12:03:00 +0100

    use module dans h_3 pour trouverElement2Dimensions

--- ALIA/ALIA/h_3.pl
+++ ALIA/ALIA/h_3.pl
@@ -1,5 +1,7 @@
 %%%Heuristique 3 : Attaque
 
+:- use_module(Puissance4).
+
 % Recuperer le meilleur coup possible C de la liste des
 % coups possibles LCP, a jouer dans la grille G dans une optique
 % d'attaque et donc d'aligner le plus grand nombre de pions possibles
@@ -15,8 +17,8 @@ attaque(G,[C1|LCP],J,C):-parcoursLCP(G,[C1|LCP],J,C1,0,C).
 % dans MAX, garder le max des deux dans MAX et stocker le coup associe
 % dans CMAX. Lorsque LCP est vide mettre CMAX dans Cf. Meme chose si on
 % a déjà trois pions alignés dans un sens (CMAX= 30).
-parcoursLCP(_,_,_,[X,Y],30,[Xf,Yf]):- Xf is X, Yf is Y.
-parcoursLCP(_,[],_,[X,Y],_,[Xf,Yf]):-Xf is X,Yf is Y.
+parcoursLCP(_,_,_,[X|Y],30,[Xf,Yf]):- Xf is X, Yf is Y.
+parcoursLCP(_,[],_,[X|Y],_,[Xf,Yf]):-Xf is X,Yf is Y.
 parcoursLCP(G,[C|LCP],J,_,MAX,Cf):- parcoursDirections(G,J,C,M),M>MAX,parcoursLCP(G,LCP,J,C,M,Cf).
 parcoursLCP(G,[C|LCP],J,CMAX,MAX,Cf):- parcoursDirections(G,J,C,M),M=<MAX,parcoursLCP(G,LCP,J,CMAX,MAX,Cf).
 
@@ -43,9 +45,9 @@ calculMax(M1,M2,M3,M4,M5,M6,M7,M8,M):-max(M1,M2,Ma),max(Ma,M3,Mb),max(Mb,M4,Mc),
 % placés à côté de la case de coordonnées [X][Y], avec le resultat mis
 % dans M1.
 parcoursLigneGD(_,_,_,3,M1):- M1 is 30.
-parcoursLigneGD(_,_,[_,Y],CMT,M1):-Y==6,M1 is CMT*10.
-parcoursLigneGD(G,J,[X,Y],CMT,M1):-Y\==6,Y1 is Y+1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneGD(G,J,[X,Y1],CMT1,M1).
-parcoursLigneGD(G,J,[X,Y],CMT,M1):-Y\==6,Y1 is Y+1,trouverElement2Dimensions(X,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursLigneGD(_,_,[_|Y],CMT,M1):-(Y==[6];Y==6),M1 is CMT*10.
+parcoursLigneGD(G,J,[X|Y],CMT,M1):-(Y\==[6];Y\==6),Y1 is Y+1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneGD(G,J,[X|Y1],CMT1,M1).
+parcoursLigneGD(G,J,[X|Y],CMT,M1):-(Y\==[6];Y\==6),Y1 is Y+1,trouverElement2Dimensions(X,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Ligne Droite Gauche DG
 % Parcourir la Xeme ligne de la grille G, en se deplacant a partir de la
@@ -53,9 +55,9 @@ parcoursLigneGD(G,J,[X,Y],CMT,M1):-Y\==6,Y1 is Y+1,trouverElement2Dimensions(X,Y
 % pour voir le nombre de pions appartenant au joueur J, consecutivement
 % placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursLigneDG(_,_,_,3,M1):- M1 is 30.
-parcoursLigneDG(_,_,[_,Y],CMT,M1):-Y==0,M1 is CMT*10.
-parcoursLigneDG(G,J,[X,Y],CMT,M1):-Y\==0,Y1 is Y-1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneDG(G,J,[X,Y1],CMT1,M1).
-parcoursLigneDG(G,J,[X,Y],CMT,M1):-Y\==0,Y1 is Y-1,trouverElement2Dimensions(X,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursLigneDG(_,_,[_|Y],CMT,M1):-(Y==[0];Y==0),M1 is CMT*10.
+parcoursLigneDG(G,J,[X|Y],CMT,M1):-(Y\==[0];Y\==0),Y1 is Y-1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneDG(G,J,[X|Y1],CMT1,M1).
+parcoursLigneDG(G,J,[X|Y],CMT,M1):-(Y\==[0];Y\==0),Y1 is Y-1,trouverElement2Dimensions(X,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Colonne Haut Bas HB
 % Parcourir la Yeme colonne de la grille G, en se deplacant a partir de
@@ -63,9 +65,9 @@ parcoursLigneDG(G,J,[X,Y],CMT,M1):-Y\==0,Y1 is Y-1,trouverElement2Dimensions(X,Y
 % CMT, pour voir le nombre de pions appartenant au joueur J, consecutivement
 % placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursColonneHB(_,_,_,3,M1):- M1 is 30.
-parcoursColonneHB(_,_,[X,_],CMT,M1):-X==5,M1 is CMT*10.
-parcoursColonneHB(G,J,[X,Y],CMT,M1):-X\==5,X1 is X+1,trouverElement2Dimensions(X1,Y,R,G),R==J,CMT1 is CMT +1,parcoursColonneHB(G,J,[X1,Y],CMT1,M1).
-parcoursColonneHB(G,J,[X,Y],CMT,M1):-X\==5,X1 is X+1,trouverElement2Dimensions(X1,Y,R,G),R\==J, M1 is CMT*10.
+parcoursColonneHB(_,_,[X|_],CMT,M1):-(X==[5];X==5),M1 is CMT*10.
+parcoursColonneHB(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),X1 is X+1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursColonneHB(G,J,[X1|Y1],CMT1,M1).
+parcoursColonneHB(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),X1 is X+1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Colonne Bas Haut BH
 % Parcourir la Yeme colonne de la grille G, en se deplacant a partir de
@@ -73,9 +75,9 @@ parcoursColonneHB(G,J,[X,Y],CMT,M1):-X\==5,X1 is X+1,trouverElement2Dimensions(X
 % CMT, pour voir le nombre de pions appartenant au joueur J, consecutivement
 % placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursColonneBH(_,_,_,3,M1):- M1 is 30.
-parcoursColonneBH(_,_,[X,_],CMT,M1):-X==0,M1 is CMT*10.
-parcoursColonneBH(G,J,[X,Y],CMT,M1):-X\==0,X1 is X-1,trouverElement2Dimensions(X1,Y,R,G),R==J,CMT1 is CMT +1,parcoursColonneBH(G,J,[X1,Y],CMT1,M1).
-parcoursColonneBH(G,J,[X,Y],CMT,M1):-X\==0,X1 is X-1,trouverElement2Dimensions(X1,Y,R,G),R\==J, M1 is CMT*10.
+parcoursColonneBH(_,_,[X|_],CMT,M1):-(X==[0];X==0),M1 is CMT*10.
+parcoursColonneBH(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),X1 is X-1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursColonneBH(G,J,[X1|Y1],CMT1,M1).
+parcoursColonneBH(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),X1 is X-1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Diagonale Bas Droit Haut Gauche BD HG/ Diagonale 11
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
@@ -84,9 +86,9 @@ parcoursColonneBH(G,J,[X,Y],CMT,M1):-X\==0,X1 is X-1,trouverElement2Dimensions(X
 % appartenant au joueur J, consecutivement placés à côté de la case de
 % coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale11(_,_,_,3,M1):- M1 is 30.
-parcoursDiagonale11(_,_,[X,Y],CMT,M1):-(X==0;Y==0),M1 is CMT*10.
-parcoursDiagonale11(G,J,[X,Y],CMT,M1):-X\==0,Y\==0,X1 is X-1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale11(G,J,[X1,Y1],CMT1,M1).
-parcoursDiagonale11(G,J,[X,Y],CMT,M1):-X\==0,Y\==0,X1 is X-1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursDiagonale11(_,_,[X|Y],CMT,M1):-(X==[0];X==0;Y==[0];Y==0),M1 is CMT*10.
+parcoursDiagonale11(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[0];Y\==0),X1 is X-1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale11(G,J,[X1|Y1],CMT1,M1).
+parcoursDiagonale11(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[0];Y\==0),X1 is X-1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Diagonale Bas Gauche Haut Droit BG HD/Diagonale 12
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
@@ -95,9 +97,9 @@ parcoursDiagonale11(G,J,[X,Y],CMT,M1):-X\==0,Y\==0,X1 is X-1,Y1 is Y-1,trouverEl
 % appartenant au joueur J, consecutivement placés à côté de la case de
 % coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale12(_,_,_,3,M1):- M1 is 30.
-parcoursDiagonale12(_,_,[X,Y],CMT,M1):-(X==0;Y==6),M1 is CMT*10.
-parcoursDiagonale12(G,J,[X,Y],CMT,M1):-X\==0,Y\==6,X1 is X-1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale12(G,J,[X1,Y1],CMT1,M1).
-parcoursDiagonale12(G,J,[X,Y],CMT,M1):-X\==0,Y\==6,X1 is X-1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursDiagonale12(_,_,[X|Y],CMT,M1):-(X==[0];X==0;Y==[6];Y==6),M1 is CMT*10.
+parcoursDiagonale12(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[6];Y\==6),X1 is X-1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale12(G,J,[X1|Y1],CMT1,M1).
+parcoursDiagonale12(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[6];Y\==6),X1 is X-1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
 
 %%Diagonale Haut Droit Bas Gauche HD BG/Diagonale 21
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
@@ -106,9 +108,10 @@ parcoursDiagonale12(G,J,[X,Y],CMT,M1):-X\==0,Y\==6,X1 is X-1,Y1 is Y+1,trouverEl
 % appartenant au joueur J, consecutivement placés à côté de la case de
 % coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale21(_,_,_,3,M1):- M1 is 30.
-parcoursDiagonale21(_,_,[X,Y],CMT,M1):-(X==5;Y==0),M1 is CMT*10.
-parcoursDiagonale21(G,J,[X,Y],CMT,M1):-X\==5,Y\==0,X1 is X+1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale21(G,J,[X1,Y1],CMT1,M1).
-parcoursDiagonale21(G,J,[X,Y],CMT,M1):-X\==5,Y\==0,X1 is X+1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursDiagonale21(_,_,[X|Y],CMT,M1):-(X==[5];X==5;Y==[0];Y==0),M1 is CMT*10.
+parcoursDiagonale21(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[0];Y\==0),X1 is X+1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale21(G,J,[X1|Y1],CMT1,M1).
+parcoursDiagonale21(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[0];Y\==0),X1 is X+1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
+
 
 %%Diagonale Haut Gauche Bas Droit HG BD/Diagonale 22
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
@@ -117,11 +120,12 @@ parcoursDiagonale21(G,J,[X,Y],CMT,M1):-X\==5,Y\==0,X1 is X+1,Y1 is Y-1,trouverEl
 % appartenant au joueur J, consecutivement placés à côté de la case de
 % coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale22(_,_,_,3,M1):- M1 is 30.
-parcoursDiagonale22(_,_,[X,Y],CMT,M1):-(X==5;Y==6),M1 is CMT*10.
-parcoursDiagonale22(G,J,[X,Y],CMT,M1):-X\==5,Y\==6,X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale22(G,J,[X1,Y1],CMT1,M1).
-parcoursDiagonale22(G,J,[X,Y],CMT,M1):-X\==5,Y\==6,X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
+parcoursDiagonale22(_,_,[X|Y],CMT,M1):-(X==[5];X==5;Y==[6];Y==6),M1 is CMT*10.
+parcoursDiagonale22(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[6];Y\==6),X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale22(G,J,[X1|Y1],CMT1,M1).
+parcoursDiagonale22(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[6];Y\==6),X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
 
-trouverElement2Dimensions(I,J,Y,L):- nth0(I,L,X),nth0(J,X,Y).
+
+
 
 %Test
 %parcoursDiagonale11([[0,0,0,0,1,0,1],[1,1,1,0,1,0,0],[0,0,0,0,0,1,1],[1,1,1,1,1,1,1],[0,1,0,0,1,1,1],[0,0,0,0,0,0,0]],1,[5,6],0,M).
