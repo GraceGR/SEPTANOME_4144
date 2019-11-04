228dbdaf910939e659fb35179e5981fc73ffcc18
AndreaCroc <46070171+AndreaCroc@users.noreply.github.com>
2019-11-04 17:00:34 +0100

    suppression accents commentaires

--- ALIA/ALIA/h2.pl
+++ ALIA/ALIA/h2.pl
@@ -16,7 +16,7 @@ attaque(G,LCP,J,C):-random_member(C1,LCP),parcoursLCP(G,LCP,J,C1,0,C).
 % nouvel appel, comparer la valeur max trouvee M a celle deja stockee
 % dans MAX, garder le max des deux dans MAX et stocker le coup associe
 % dans CMAX. Lorsque LCP est vide mettre CMAX dans Cf. Meme chose si on
-% a déjà trois pions alignés dans un sens (CMAX= 30).
+% a deja trois pions alignes dans un sens (CMAX= 30).
 
 parcoursLCP(_,_,_,[X|Y],30,[Xf,Yf]):- Xf is X, Yf is Y.
 parcoursLCP(_,[],_,[X|Y],_,[Xf,Yf]):-Xf is X,Yf is Y.
@@ -34,7 +34,7 @@ parcoursDirections(G,J,C,M):-parcoursLigneDG(G,J,C,0,M1),parcoursLigneGD(G,J,C,0
 max(A,B,A):-A>=B.
 max(A,B,B):-A<B.
 
-% Recuperer le max entre M1,M2,M3,M4,M5,M6,M7 et M8 mettre le
+% Recuperer le max entre M1,M2,M3,M4,M5,M6,M7,M8 et mettre le
 % resultat dans M
 calculMax(M1,M2,M3,M4,M5,M6,M7,M8,M):-max(M1,M2,Ma),max(Ma,M3,Mb),max(Mb,M4,Mc),max(Mc,M5,Md),
 	max(Md,M6,Me),max(Me,M7,Mf),max(Mf,M8,M).
@@ -43,8 +43,7 @@ calculMax(M1,M2,M3,M4,M5,M6,M7,M8,M):-max(M1,M2,Ma),max(Ma,M3,Mb),max(Mb,M4,Mc),
 % Parcourir la Xeme ligne de la grille G, en se deplacant a partir de la
 % colonne Y de trois coups vers la droite en utilisant le compteur CMT
 % pour voir le nombre de pions appartenant au joueur J, consecutivement
-% placés à côté de la case de coordonnées [X][Y], avec le resultat mis
-% dans M1.
+% places à cote de la case de coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursLigneGD(_,_,_,3,M1):- M1 is 30.
 parcoursLigneGD(_,_,[_|Y],CMT,M1):-(Y==[6];Y==6),M1 is CMT*10.
 parcoursLigneGD(G,J,[X|Y],CMT,M1):-(Y\==[6];Y\==6),Y1 is Y+1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneGD(G,J,[X|Y1],CMT1,M1).
@@ -54,7 +53,7 @@ parcoursLigneGD(G,J,[X|Y],CMT,M1):-(Y\==[6];Y\==6),Y1 is Y+1,trouverElement2Dime
 % Parcourir la Xeme ligne de la grille G, en se deplacant a partir de la
 % colonne Y de trois coups vers la gauche en utilisant le compteur CMT,
 % pour voir le nombre de pions appartenant au joueur J, consecutivement
-% placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
+% places à cote de la case de coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursLigneDG(_,_,_,3,M1):- M1 is 30.
 parcoursLigneDG(_,_,[_|Y],CMT,M1):-(Y==[0];Y==0),M1 is CMT*10.
 parcoursLigneDG(G,J,[X|Y],CMT,M1):-(Y\==[0];Y\==0),Y1 is Y-1,trouverElement2Dimensions(X,Y1,R,G),R==J,CMT1 is CMT +1,parcoursLigneDG(G,J,[X|Y1],CMT1,M1).
@@ -64,7 +63,7 @@ parcoursLigneDG(G,J,[X|Y],CMT,M1):-(Y\==[0];Y\==0),Y1 is Y-1,trouverElement2Dime
 % Parcourir la Yeme colonne de la grille G, en se deplacant a partir de
 % la ligne X de trois coups vers le bas en utilisant le compteur
 % CMT, pour voir le nombre de pions appartenant au joueur J, consecutivement
-% placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
+% places à cote de la case de coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursColonneHB(_,_,_,3,M1):- M1 is 30.
 parcoursColonneHB(_,_,[X|_],CMT,M1):-(X==[5];X==5),M1 is CMT*10.
 parcoursColonneHB(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),X1 is X+1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursColonneHB(G,J,[X1|Y1],CMT1,M1).
@@ -74,7 +73,7 @@ parcoursColonneHB(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),X1 is X+1,Y1 is Y,trouverEl
 % Parcourir la Yeme colonne de la grille G, en se deplacant a partir de
 % la ligne X de trois coups vers le haut en utilisant le compteur
 % CMT, pour voir le nombre de pions appartenant au joueur J, consecutivement
-% placés à côté de la case de coordonnées [X][Y], avec le resultat mit dans M1.
+% places à cote de la case de coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursColonneBH(_,_,_,3,M1):- M1 is 30.
 parcoursColonneBH(_,_,[X|_],CMT,M1):-(X==[0];X==0),M1 is CMT*10.
 parcoursColonneBH(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),X1 is X-1,Y1 is Y,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursColonneBH(G,J,[X1|Y1],CMT1,M1).
@@ -82,9 +81,9 @@ parcoursColonneBH(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),X1 is X-1,Y1 is Y,trouverEl
 
 %%Diagonale Bas Droit Haut Gauche BD HG/ Diagonale 11
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
-% partir de la case de coordonnées [X][Y] de trois coups vers le haut
+% partir de la case de coordonnees [X][Y] de trois coups vers le haut
 % gauche en utilisant le compteur CMT, pour voir le nombre de pions
-% appartenant au joueur J, consecutivement placés à côté de la case de
+% appartenant au joueur J, consecutivement places a cote de la case de
 % coordonnées [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale11(_,_,_,3,M1):- M1 is 30.
 parcoursDiagonale11(_,_,[X|Y],CMT,M1):-(X==[0];X==0;Y==[0];Y==0),M1 is CMT*10.
@@ -93,10 +92,10 @@ parcoursDiagonale11(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[0];Y\==0),X1 is X-1
 
 %%Diagonale Bas Gauche Haut Droit BG HD/Diagonale 12
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
-% partir de la case de coordonnées [X][Y] de trois coups vers le haut
+% partir de la case de coordonnees [X][Y] de trois coups vers le haut
 % droit en utilisant le compteur CMT, pour voir le nombre de pions
-% appartenant au joueur J, consecutivement placés à côté de la case de
-% coordonnées [X][Y], avec le resultat mit dans M1.
+% appartenant au joueur J, consecutivement places a cote de la case de
+% coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale12(_,_,_,3,M1):- M1 is 30.
 parcoursDiagonale12(_,_,[X|Y],CMT,M1):-(X==[0];X==0;Y==[6];Y==6),M1 is CMT*10.
 parcoursDiagonale12(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[6];Y\==6),X1 is X-1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale12(G,J,[X1|Y1],CMT1,M1).
@@ -104,10 +103,10 @@ parcoursDiagonale12(G,J,[X|Y],CMT,M1):-(X\==[0];X\==0),(Y\==[6];Y\==6),X1 is X-1
 
 %%Diagonale Haut Droit Bas Gauche HD BG/Diagonale 21
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
-% partir de la case de coordonnées [X][Y] de trois coups vers le bas
+% partir de la case de coordonnees [X][Y] de trois coups vers le bas
 % gauche en utilisant le compteur CMT, pour voir le nombre de pions
-% appartenant au joueur J, consecutivement placés à côté de la case de
-% coordonnées [X][Y], avec le resultat mit dans M1.
+% appartenant au joueur J, consecutivement places a cote de la case de
+% coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale21(_,_,_,3,M1):- M1 is 30.
 parcoursDiagonale21(_,_,[X|Y],CMT,M1):-(X==[5];X==5;Y==[0];Y==0),M1 is CMT*10.
 parcoursDiagonale21(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[0];Y\==0),X1 is X+1,Y1 is Y-1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale21(G,J,[X1|Y1],CMT1,M1).
@@ -116,12 +115,11 @@ parcoursDiagonale21(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[0];Y\==0),X1 is X+1
 
 %%Diagonale Haut Gauche Bas Droit HG BD/Diagonale 22
 % Parcourir la diagonale (X,Y) de la grille G, en se deplacant a
-% partir de la case de coordonnées [X][Y] de trois coups vers le bas
+% partir de la case de coordonnees [X][Y] de trois coups vers le bas
 % droit en utilisant le compteur CMT, pour voir le nombre de pions
-% appartenant au joueur J, consecutivement placés à côté de la case de
-% coordonnées [X][Y], avec le resultat mit dans M1.
+% appartenant au joueur J, consecutivement places a cote de la case de
+% coordonnees [X][Y], avec le resultat mit dans M1.
 parcoursDiagonale22(_,_,_,3,M1):- M1 is 30.
 parcoursDiagonale22(_,_,[X|Y],CMT,M1):-(X==[5];X==5;Y==[6];Y==6),M1 is CMT*10.
 parcoursDiagonale22(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[6];Y\==6),X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R==J,CMT1 is CMT +1,parcoursDiagonale22(G,J,[X1|Y1],CMT1,M1).
 parcoursDiagonale22(G,J,[X|Y],CMT,M1):-(X\==[5];X\==5),(Y\==[6];Y\==6),X1 is X+1,Y1 is Y+1,trouverElement2Dimensions(X1,Y1,R,G),R\==J, M1 is CMT*10.
-
