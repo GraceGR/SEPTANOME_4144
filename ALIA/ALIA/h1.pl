9b81e9cc228ee7f1059dc9bfe66dad301ae8f44d
GraceGR <46426990+GraceGR@users.noreply.github.com>
2019-11-06 08:36:31 +0100

    Update h1.pl

--- ALIA/ALIA/h1.pl
+++ ALIA/ALIA/h1.pl
@@ -1,95 +1,113 @@
-
+:- use_module(library(clpfd)).
 
 :-use_module(puissance4).
 
+%RÃƒÂ©cupÃƒÂ©rer l'ÃƒÂ©lÃƒÂ©ment d'un liste ÃƒÂ  l'indice i
+trouverElement(1,X,[X|_]).
+trouverElement(N1,X,[_|L]) :- trouverElement(N,X,L), N1 is N+1.
 
-%X et Y sont les cordonnees de la case, L la grille de jeu et R le resultat
-voisin_gauche([X,Y2],R, L):-trouverElement2Dimensions(X,Y,R,L),Y2 is Y+1, X<6, X>=0, Y<6.
-voisin_gauche([_,0],R, L):-R is 404.
+%RÃ©cupÃ©rer un Ã©lement d'une liste L Ã  deux dimensions Ã  l'indice [I][J]et le rÃ©sultat va dans Y
+% trouverElement2Dimensions(I,J,Y,L):-trouverElement(I,X,L),trouverElement(J,Y,X).
+trouverElement2Dimensions(I,J,Y,L):- nth0(I,L,X),nth0(J,X,Y).
 
-%X et Y sont les cordonnees de la case, L la grille de jeu et R le resultat
-voisin_droite([X,Y2],R, L):-trouverElement2Dimensions(X,Y,R,L),Y2 is Y-1, X<6, Y=<7 .
-voisin_droite([_,6],R, L):-R is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_gauche([X,Y2],R, G):-trouverElement2Dimensions(X,Y,R,G),Y2 is Y+1, X<6, X>=0, Y<6,!.
+voisin_gauche([_,0],R, _):-R is 404.
 
-%X et Y sont les cordonnees de la case, L la grille de jeu et R le resultat
-voisin_haut([X2,Y],R, L):-trouverElement2Dimensions(X,Y,R,L),X2 is X+1, X<5,Y<7, X2>0.
-voisin_haut([0,_],R, L):-R is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_droite([X,Y2],R, G):-trouverElement2Dimensions(X,Y,R,G),Y2 is Y-1, X<6, Y=<7,! .
+voisin_droite([_,6],R, _):-R is 404.
 
+%X et Y sont les cordonnees de la case, G la grille de jeu et R le resultat
+%404 est renvoyé si la case est sur le bord
+voisin_bas([X2,Y],R, G):-trouverElement2Dimensions(X,Y,R,G),X2 is X-1, X>=0,Y>=0,!.
+voisin_bas([5,_],R, _):-R is 404.
 
-%X et Y sont les cordonnees de la case, L la grille de jeu et R le resultat
-voisin_bas([X2,Y],R, L):-trouverElement2Dimensions(X,Y,R,L),X2 is X-1, X<6, Y<6, X2>0, X>0.
-voisin_bas([5,_],R, L):-R is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_haut([X,Y],R, G):-trouverElement2Dimensions(X2,Y,R,G),X2 is X-1, X<6, Y<7, X>0,!.
+voisin_haut([0,_],R, _):-R is 404.
+%voisin_bas([X2,Y],R, G):-trouverElement2Dimensions(X,Y,R,G),X2 is X-1, X<6, Y<7, X2>0.
+%voisin_bas([5,_],R, _):-R is 404.
 
 %La valeur du 1er voisin NO
-%X et Y sont les cordonnees de la case, L la grille de jeu et R2 le resultat
-voisin_NordOuest([X,Y],R2, L):-trouverElement2Dimensions(X2,Y2,R,L), X2 is X-1, Y2 is Y-1, X2>=0, Y2>=0, R2 is R.
-voisin_NordOuest([X,Y],R2, L):- (  X==0, Y==0), R2 is 404.
-voisin_NordOuest([X,Y],R2, L):- ( X=<0; Y=<0), (  X=\=0; Y=\=0),R2 is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R2 le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_NordOuest([X,Y],R2, G):-trouverElement2Dimensions(X2,Y2,R,G), X2 is X-1, Y2 is Y-1, X2>=0, Y2>=0, R2 is R,!.
+voisin_NordOuest([X,Y],R2, _):- (  X==0, Y==0), R2 is 404.
+voisin_NordOuest([X,Y],R2, _):- ( X=<0; Y=<0), (  X=\=0; Y=\=0),R2 is 404.
 
 %La valeur du 1er voisin NE
-%X et Y sont les cordonnees de la case, L la grille de jeu et R2 le resultat
-voisin_NordEst([X,Y],R2, L):-trouverElement2Dimensions(X2,Y2,R,L), X2 is X-1, Y2 is Y+1, X2>=0, Y2=<7, R2 is R.
-voisin_NordEst([0,Y],R2, L):- R2 is 404.
-voisin_NordEst([X,6],R2, L):- X=\=0, R2 is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R2 le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_NordEst([X,Y],R2, G):-trouverElement2Dimensions(X2,Y2,R,G), X2 is X-1, Y2 is Y+1, X2>=0, Y2=<7, R2 is R,!.
+voisin_NordEst([0,_],R2, _):- R2 is 404.
+voisin_NordEst([X,6],R2, _):- X=\=0, R2 is 404.
 
 
 %La valeur du voisin en bas a droite d'une case
-%X et Y sont les cordonnees de la case, L la grille de jeu et R2 le resultat
-voisin_SudEst([X,Y],R2, L):-trouverElement2Dimensions(X2,Y2,R,L), X2 is X+1, Y2 is Y+1, X2=<7, Y2=<7, R2 is R.
-voisin_SudEst([5,Y],R2, L):- R2 is 404.
-voisin_SudEst([X,6],R2, L):- X=\=5, R2 is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R2 le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_SudEst([X,Y],R2, G):-trouverElement2Dimensions(X2,Y2,R,G), X2 is X+1, Y2 is Y+1, X2=<7, Y2=<7, R2 is R,!.
+voisin_SudEst([5,_],R2, _):- R2 is 404.
+voisin_SudEst([X,6],R2, _):- X=\=5, R2 is 404.
 
 %La valeur du voisin en bas a gauche d'une case
-%X et Y sont les cordonnees de la case, L la grille de jeu et R2 le resultat
-voisin_SudOuest([X,Y],R2, L):-trouverElement2Dimensions(X2,Y2,R,L), X2 is X+1, Y2 is Y-1, X2=<7, Y2>=0, R2 is R.
-voisin_SudOuest([5,Y],R2, L):- R2 is 404.
-voisin_SudOuest([X,0],R2, L):- X=\=5, R2 is 404.
+%X et Y sont les cordonnees de la case, G la grille de jeu et R2 le resultat
+%%404 est renvoyé si la case est sur le bord
+voisin_SudOuest([X,Y],R2, G):-trouverElement2Dimensions(X2,Y2,R,G), X2 is X+1, Y2 is Y-1, X2=<7, Y2>=0, R2 is R,!.
+voisin_SudOuest([5,_],R2, _):- R2 is 404.
+voisin_SudOuest([X,0],R2, _):- X=\=5, R2 is 404.
 
 
-%Retourne 1 s il y a 1 pion de la meme couleur en haut ou en bas, 2 si les 2.
-compter_voisins_mCouleur_HB([X,Y],L,R2,J):-( voisin_haut([X,Y],R3, L) , voisin_bas([X,Y],R4, L)) , (   R3=\=J , R4=\=J  ),R2 is 0.
-compter_voisins_mCouleur_HB([X,Y],L,R2,J):-( voisin_haut([X,Y],R3, L),voisin_bas([X,Y],R4, L)) , (   R3==J , R4==J),R2 is 2.
-compter_voisins_mCouleur_HB([X,Y],L,R2,J):-( voisin_haut([X,Y],R3, L) , voisin_bas([X,Y],R4, L)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1.
+%Retourne 1 s il y a 1 pion de la meme couleur que le joueur (1 ou 2) en haut ou en bas, 2 si les 2.
+compter_voisins_mCouleur_HB([X,Y],G,R2,J):-( voisin_haut([X,Y],R3, G) , voisin_bas([X,Y],R4, G)) , (   R3=\=J , R4=\=J  ),R2 is 0,!.
+compter_voisins_mCouleur_HB([X,Y],G,R2,J):-( voisin_haut([X,Y],R3, G),voisin_bas([X,Y],R4, G)) , (   R3==J , R4==J),R2 is 2,!.
+compter_voisins_mCouleur_HB([X,Y],G,R2,J):-( voisin_haut([X,Y],R3, G) , voisin_bas([X,Y],R4, G)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1,!.
 
 
 %Retourne 1 s il y a 1 pion de la meme couleur a gauche ou a droite, 2 si les 2.
-compter_voisins_mCouleur_GD([X,Y],L,R2,J):-( voisin_droite([X,Y],R3, L),voisin_gauche([X,Y],R4, L)) , (   R3=\=J , R4=\=J ),R2 is 0.
-compter_voisins_mCouleur_GD([X,Y],L,R2,J):-( voisin_droite([X,Y],R3, L),voisin_gauche([X,Y],R4, L)) , (   R3==J , R4==J),R2 is 2.
-compter_voisins_mCouleur_GD([X,Y],L,R2,J):-( voisin_droite([X,Y],R3, L),voisin_gauche([X,Y],R4, L)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1.
+compter_voisins_mCouleur_GD([X,Y],G,R2,J):-( voisin_droite([X,Y],R3, G),voisin_gauche([X,Y],R4, G)) , (   R3=\=J , R4=\=J ),R2 is 0,!.
+compter_voisins_mCouleur_GD([X,Y],G,R2,J):-( voisin_droite([X,Y],R3, G),voisin_gauche([X,Y],R4, G)) , (   R3==J , R4==J),R2 is 2,!.
+compter_voisins_mCouleur_GD([X,Y],G,R2,J):-( voisin_droite([X,Y],R3, G),voisin_gauche([X,Y],R4, G)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1,!.
 
 %Pareil sur la diagonale ascendente de droite a gauche.
-compter_voisins_mCouleur_Diag1([X,Y],L,R2,J):-( voisin_SudOuest([X,Y],R3, L),voisin_NordEst([X,Y],R4, L)) , (   R3=\=J , R4=\=J ),R2 is 0.
-compter_voisins_mCouleur_Diag1([X,Y],L,R2,J):-( voisin_SudOuest([X,Y],R3, L),voisin_NordEst([X,Y],R4, L)) , (   R3==J , R4==J),R2 is 2.
-compter_voisins_mCouleur_Diag1([X,Y],L,R2,J):-( voisin_SudOuest([X,Y],R3, L),voisin_NordEst([X,Y],R4, L)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1.
+compter_voisins_mCouleur_Diag1([X,Y],G,R2,J):-( voisin_SudOuest([X,Y],R3, G),voisin_NordEst([X,Y],R4, G)) , (   R3=\=J , R4=\=J ),R2 is 0,!.
+compter_voisins_mCouleur_Diag1([X,Y],G,R2,J):-( voisin_SudOuest([X,Y],R3, G),voisin_NordEst([X,Y],R4, G)) , (   R3==J , R4==J),R2 is 2,!.
+compter_voisins_mCouleur_Diag1([X,Y],G,R2,J):-( voisin_SudOuest([X,Y],R3, G),voisin_NordEst([X,Y],R4, G)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1,!.
 
 
 %Pareil sur la diagonale descendente de droite a gauche.
-compter_voisins_mCouleur_Diag2([X,Y],L,R2,J):-( voisin_SudEst([X,Y],R3, L),voisin_NordOuest([X,Y],R4, L)) , (   R3=\=J , R4=\=J  ),R2 is 0.
-compter_voisins_mCouleur_Diag2([X,Y],L,R2,J):-( voisin_SudEst([X,Y],R3, L),voisin_NordOuest([X,Y],R4, L)) , (   R3==J , R4==J),R2 is 2.
-compter_voisins_mCouleur_Diag2([X,Y],L,R2,J):-( voisin_SudEst([X,Y],R3, L),voisin_NordOuest([X,Y],R4, L)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1.
+compter_voisins_mCouleur_Diag2([X,Y],G,R2,J):-( voisin_SudEst([X,Y],R3, G),voisin_NordOuest([X,Y],R4, G)) , (   R3=\=J , R4=\=J  ),R2 is 0,!.
+compter_voisins_mCouleur_Diag2([X,Y],G,R2,J):-( voisin_SudEst([X,Y],R3, G),voisin_NordOuest([X,Y],R4, G)) , (   R3==J , R4==J),R2 is 2,!.
+compter_voisins_mCouleur_Diag2([X,Y],G,R2,J):-( voisin_SudEst([X,Y],R3, G),voisin_NordOuest([X,Y],R4, G)) , (  ( (R3=\=J ; R4=\=J)) , (   R3==J ; R4==J )) ,R2 is 1,!.
 
 % Compte le nombre total de voisins de la meme couleur que le joueur
-% autour d'une case donnee X et Y sont les coordonnees de la case, L la
+% autour d'une case donnee X et Y sont les coordonnees de la case, G la
 % grille de jeu, J le joueur (1 ou 2) et R le resultat
-nb_voisins_total([X,Y],L,R,J):-compter_voisins_mCouleur_HB([X,Y],L,R2,J),compter_voisins_mCouleur_GD([X,Y],L,R3,J),
-							compter_voisins_mCouleur_Diag1([X,Y],L,R4,J), compter_voisins_mCouleur_Diag2([X,Y],L,R5,J),R is R2+R3+R4+R5 .
+nb_voisins_total([X,Y],G,R,J):-compter_voisins_mCouleur_HB([X,Y],G,R2,J),compter_voisins_mCouleur_GD([X,Y],G,R3,J),
+							compter_voisins_mCouleur_Diag1([X,Y],G,R4,J), compter_voisins_mCouleur_Diag2([X,Y],G,R5,J),R is R2+R3+R4+R5 .
 
 % Donne la liste des nombres de voisins de la meme couleur que le joueur
-% pour tout les coups possibles A est la LCP, L la grille de jeu, J le
+% pour tout les coups de la LCP, G la grille de jeu, J le
 % joueur et C la liste resultat
-liste_nbVoisins([A],L,J,[C]):-nb_voisins_total(A,L,C,J).
-liste_nbVoisins([A|Y],L,J,[C|R]):-nb_voisins_total(A,L,C,J),liste_nbVoisins(Y,L,J,R).
+liste_nbVoisins([],_,_,C):-C is 0.
+liste_nbVoisins([LCP],G,J,[C]):-nb_voisins_total(LCP,G,C,J).
+liste_nbVoisins([A|LCP],G,J,[C|R]):-nb_voisins_total(A,G,C,J),liste_nbVoisins(LCP,G,J,R),!.
 
-%recuperer le max d'un tableau
-max([],0).
-max([H|T],Max) :-max(T,TMax),H > TMax,Max is H.
-max([H|T],Max) :- max(T,TMax), H=< TMax, Max is TMax.
+%recuperer le max d'un tableau et son indice
+indice_et_maximum(LCP,Max,End) :-
+   maplist(#>=(Max),LCP),
+   nth0(End,LCP,Max).
 
-%recuperer l'element X d'indice M d'un tableau
-indice(X, 0, [X | _]).
-indice(X, M, [_ | L]) :- indice(X, K, L), M is K + 1.
+%recupère l'indice du coup choisi dans la LCP
+%G est la grille de jeu, J le joueur(1 ou 2), Ind1 l'indice renvoyé
+indice_coup_choisi([],_,_,Ind1):-Ind1 is 0.
+indice_coup_choisi(LCP,G,J,Ind1):-liste_nbVoisins(LCP,G,J,C),indice_et_maximum(C,_,Ind), Ind1 is Ind+1,!.
 
-indice_coup_choisi(A,L,J,K):-liste_nbVoisins(A,L,J,C), max(C,I2),   indice(I2, K, C),!.
-
-heuristique1(L,A,J,C3):-indice_coup_choisi(A,L,J,K),trouverElement(K,C3,A),!.
+%l'heuristique en elle même qui donne le coup à jouer.
+heuristique1(_,[],_,C3):-C3 is 0.
+heuristique1(G,LCP,J,C3):-indice_coup_choisi(LCP,G,J,K),trouverElement(K,C3,LCP),!.
 
