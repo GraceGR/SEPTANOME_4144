48ac99e0f9595c06c68af0e86761f110a0b76a9a
Alexxx205 <32546254+Alexxx205@users.noreply.github.com>
2019-11-06 09:12:25 +0100

    Tests heuristiques h1, h2, h3, h4
    Ajout des tests de predicats pour h1

--- ALIA/ALIA/TestHeuristiques.pl
+++ ALIA/ALIA/TestHeuristiques.pl
@@ -1,7 +1,202 @@
 :-use_module(puissance4).
+:- use_module(h1).
 :-use_module(h2).
 :-use_module(h3).
 :-use_module(h4).
+
+%%% Test predicats heuristique 1 %%%
+
+%Test predicat voisin_gauche
+testVoisinGauche(N):- voisin_gauche([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=1
+
+testVoisinGauche2(N):- voisin_gauche([3,0],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+%Test predicat voisin_droite
+testVoisinDroite(N) :- voisin_droite([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,5,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisinDroite2(N) :- voisin_droite([3,6],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+%Test predicat voisin_haut
+testVoisinHaut(N):- voisin_haut([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,5,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisinHaut2(N):- voisin_haut([0,3],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+%Test predicat voisin_bas
+testVoisinBas(N) :- voisin_bas([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,5,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisinBas2(N) :- voisin_bas([5,3],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+
+%Test predicat voisin_nordOuest
+testVoisin_NordOuest(N):-voisin_NordOuest([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisin_NordOuest2(N):-voisin_NordOuest([0,0],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordOuest3(N):-voisin_NordOuest([0,1],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordOuest4(N):-voisin_NordOuest([1,0],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordOuest5(N):-voisin_NordOuest([1,1],N,[[6,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=6
+
+
+%Test predicat voisin_nordEst
+testVoisin_NordEst(N):-voisin_NordEst([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,5,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisin_NordEst2(N):-voisin_NordEst([0,6],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordEst3(N):-voisin_NordEst([0,5],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordEst4(N):-voisin_NordEst([1,6],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_NordEst5(N):-voisin_NordEst([1,5],N,[[0,0,0,0,0,0,6],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=6
+
+
+%Test predicat voisin_sudOuest
+testVoisin_SudOuest(N):-voisin_SudOuest([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisin_SudOuest2(N):-voisin_SudOuest([5,0],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudOuest3(N):-voisin_SudOuest([5,1],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudOuest4(N):-voisin_SudOuest([4,0],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudOuest5(N):-voisin_SudOuest([4,1],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[6,1,2,1,2,0,0]]).
+%Resultat attendu : N=6
+
+
+%Test predicat voisin_sudEst
+testVoisin_SudEst(N):-voisin_SudEst([3,4],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,5,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=5
+
+testVoisin_SudEst2(N):-voisin_SudEst([5,5],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudEst3(N):-voisin_SudEst([5,6],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudEst4(N):-voisin_SudEst([4,6],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,5,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]]).
+%Resultat attendu : N=404
+
+testVoisin_SudEst5(N):-voisin_SudEst([4,5],N,[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,0,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,5]]).
+%Resultat attendu : N=5
+
+
+%Test predicat compter_voisins_mCouleur_HB
+testCvmcHB(X):-compter_voisins_mCouleur_HB([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,0,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=0
+
+testCvmcHB1(X):-compter_voisins_mCouleur_HB([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=1
+
+testCvmcHB2(X):-compter_voisins_mCouleur_HB([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,2,1,2,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=2
+
+
+%Test predicat compter_voisins_mCouleur_GD
+testCvmcGD(X):-compter_voisins_mCouleur_GD([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,0,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=0
+
+testCvmcGD1(X):-compter_voisins_mCouleur_GD([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,2,1,1,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=1
+
+testCvmcGD2(X):-compter_voisins_mCouleur_GD([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=2
+
+
+%Test predicat compter_voisins_mCouleur_Diag1
+testCvmc1D(X):-compter_voisins_mCouleur_Diag1([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,0,2,0,0],[0,1,2,1,2,0,0],[0,2,2,2,2,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=0
+
+testCvmc1D1(X):-compter_voisins_mCouleur_Diag1([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,2,1,1,0,0],[0,2,2,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=1
+
+testCvmc1D2(X):-compter_voisins_mCouleur_Diag1([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=2
+
+
+%Test predicat compter_voisins_mCouleur_Diag2
+testCvmc2D(X):-compter_voisins_mCouleur_Diag2([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,0,1,0,0],[0,1,2,1,2,0,0],[0,2,1,2,2,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=0
+
+testCvmc2D1(X):-compter_voisins_mCouleur_Diag2([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,2,1,1,0,0],[0,1,2,1,1,0,0],[0,2,1,2,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=1
+
+testCvmc2D2(X):-compter_voisins_mCouleur_Diag2([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],X,1).
+%Resultat attendu : X=2
+
+
+%Test predicat nb_voisins_total
+testNbVoisinsTotal(N):-nb_voisins_total([3,3],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],N,1).
+%Resultat attendu : N=8
+
+testNbVoisinsTotal2(N):-nb_voisins_total([0,0],[[0,1,0,0,0,0,0],[1,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],N,1).
+%Resultat attendu : N=2
+
+testNbVoisinsTotal3(N):-nb_voisins_total([0,6],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],N,1).
+%Resultat attendu : N=0
+
+testNbVoisinsTotal4(N):-nb_voisins_total([5,0],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[1,1,1,1,1,0,0],[0,1,2,1,2,0,0]],N,1).
+%Resultat attendu : N=3
+
+testNbVoisinsTotal5(N):-nb_voisins_total([5,6],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,2,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,1,0],[0,1,2,1,2,0,0]],N,1).
+%Resultat attendu : N=1
+
+
+%Test predicat liste_nbVoisins
+testListeNbVoisins(Res):-liste_nbVoisins([[0,1],[2,0],[3,3]],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],1,Res).
+%Resultat attendu : Res=[0,1,8]
+
+testListeNbVoisins2(Res):-liste_nbVoisins([],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],1,Res).
+%Resultat attendu : Res=0
+
+
+%Test predicat indice_coup_choisi
+testIndCoupChoisi(X):-indice_coup_choisi([[0,1],[2,0],[3,3]],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,1,1,1,1,0,0],[0,2,1,1,1,0,0],[0,1,2,1,2,0,0]],1,X).
+%Resultat attendu : X=3
+
+testIndCoupChoisi2(X):-indice_coup_choisi([[0,6],[1,4],[0,0],[1,2]],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,2,1,2,2,0,0],[0,2,2,2,2,0,0],[0,1,2,1,2,0,0]],1,X).
+%Resultat attendu : X=2
+
+testIndCoupChoisi3(X):-indice_coup_choisi([],[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,2,1,2,2,0,0],[0,2,2,2,2,0,0],[0,1,2,1,2,0,0]],1,X).
+%Resultat attendu : X=0
+
+
+%Test predicat heuristique1
+testH1(X):-heuristique1([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,1,1,0,0],[0,2,1,2,2,0,0],[0,2,2,2,2,0,0],[0,1,2,1,2,0,0]],[[0,6],[1,4],[0,0],[1,2]],1,X).
+%Resultat attendu : X=[1,4]
+
+testH12(X):-heuristique1([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],[[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6]],1,X).
+%Resultat attendu : X=[5,0]
+
+testH13(X):-heuristique1([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],[[5,0]],1,X).
+%Resultat attendu : X=[5,0]
+
+testH14(X):-heuristique1([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],[],1,X).
+%Resultat attendu : X=0
+
+
 
 %%% Test predicats heuristique 2 %%%
 
